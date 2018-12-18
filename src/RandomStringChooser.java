cd325504d823c5997bd4ce8a468504e31b1c977a
BT_1N3_06 <mgyawu0974@bths.edu>
2018-12-18 11:43:03 -0500

    Dareen Yau is an awesome dude

--- src/RandomStringChooser.java
+++ src/RandomStringChooser.java
@@ -1,2 +1,17 @@
 public class RandomStringChooser {
+
+    private String[] arrays;
+
+    public RandomStringChooser(String[] arrays){
+        this.arrays = arrays;
+    }
+
+    public String getNext(){
+        int index = (int)(Math.random()*this.arrays.length);
+        String currentString = "";
+        if(this.arrays[index] == null){
+            return "NONE";
+        }
+
+    }
 }
